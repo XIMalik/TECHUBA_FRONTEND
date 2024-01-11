45d9b77988a482461dc1025e4c08b7c767db04ce
Lekan <abdulmalikawesu@gmail.com>
2024-01-11 11:57:31 +0100

    fixed landing request endpoint

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -10,7 +10,7 @@ const Landing = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/'); 
+        const response = await axios.get('https://rldondon.pythonanywhere.com/'); 
         console.log('Data fetched successfully:', response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
